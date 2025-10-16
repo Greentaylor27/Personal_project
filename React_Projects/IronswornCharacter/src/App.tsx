c2b07368120346485db86693321559b3a2754be5
Greentaylor27 <greentaylor18@gmail.com>
2025-10-16 13:49:52 -0500

    Starting a new project and have added my first component.

---
+++ React_Projects/IronswornCharacter/src/App.tsx
@@ -0,0 +1,14 @@
+import MomentumTracker from './components/MomentumTracker'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello World!</p>
+      <MomentumTracker />
+    </>
+  )
+}
+
+export default App
